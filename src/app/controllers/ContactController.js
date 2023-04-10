c879a2f9d8f05e48bdaaa1b77fdfc17c69196dca
under-sin <andersonvieira818@yahoo.com.br>
2023-04-10 13:54:16 -0300

    first commit

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,24 @@
+class ContactController {
+  index() {
+    // listar todos os registros
+  }
+
+  show() {
+    // obterr um registro
+  }
+
+  store() {
+    // criar um novo registro
+  }
+
+  update() {
+    // editar um registro
+  }
+
+  delelte() {
+    // deletar um registro
+  }
+}
+
+// singleton => retorna apenas uma instância da controller
+module.exports = new ContactController();
