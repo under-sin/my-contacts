fcf7b562bb48bba6e52b70564e55739cc1f9a4ab
under-sin <andersonvieira818@yahoo.com.br>
2023-04-11 20:25:43 -0300

    Finalizando update

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -34,7 +34,7 @@ class ContactController {
     if (contactExists) {
       return response
         .status(400)
-        .json({ error: "This e-mail is already been taken" });
+        .json({ error: "This e-mail is already in use" });
     }
 
     const contact = await ContactsRepository.create({
@@ -47,8 +47,37 @@ class ContactController {
     response.send(contact);
   }
 
-  update() {
+  async update(request, response) {
     // editar um registro
+    const { id } = request.params;
+    const { name, email, phone, category_id } = request.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(400).json({ error: "Contact not found" });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: "Name is required" });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return response
+        .status(400)
+        .json({ error: "This e-mail is already in use" });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
