b8c08fa44e5225551c90eaf837c690943002a99b
under-sin <andersonvieira818@yahoo.com.br>
2023-04-16 10:17:38 -0300

    Conectando nodejs ao banco de dados

---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+const { Client } = require("pg");
+
+// conectando ao banco de dados
+const client = new Client({
+  host: "localhost",
+  port: 5432,
+  user: "root",
+  password: "root",
+  database: "mycontacts",
+});
+
+client.connect();
+
+// client.query -> retorna uma promisse
+exports.query = async (query) => {
+  // dentro  da query os unicos dados que precisamos são as rows
+  const { rows } = await client.query(query);
+  return rows;
+};
+
+// Query("SELECT * FROM contacts").then((result) => console.log(result));
