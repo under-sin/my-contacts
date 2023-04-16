408d9d64d0086c61e7434bb8445bdd060de18045
under-sin <andersonvieira818@yahoo.com.br>
2023-04-16 14:54:27 -0300

    Finalizando a API com a adição dos erros handles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+// lib que permite usar o error handler em métodos async
+require("express-async-errors");
+
 const router = require("./router");
 
 const app = express();
@@ -7,5 +10,12 @@ const app = express();
 // middleware que monta o body da requisição (não precisa fazer na mão)
 app.use(express.json());
 app.use(router);
+
+// error handler precisa ficar depois da definação das rotas.
+app.use((error, resquest, response, next) => {
+  console.log("----- error handler -----");
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log("Server started at http://localhost:3000"));
